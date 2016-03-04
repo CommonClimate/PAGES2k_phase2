df461a6a7800e53f080e915fb3fbc4d4a96b65ae
ElNino <julieneg@usc.edu>
2016-03-04 13:59:42 -0800

    1.9.0 near final
    cleaned up figure file name so that all have the same prefix, including
    database version.  removed outdated figures and data files.

--- pages2k_compositeByLatitudeBand.m
+++ pages2k_compositeByLatitudeBand.m
@@ -20,28 +20,42 @@ band(2).title = '30N to 60N';
 band(1).index = find(lat>=60 & lat<=90);
 band(1).title = '60N to 90N';
 
+nmin = 5 ; %minimum number of records
 fig('By latitude band'), clf
 hold on
 for i = 1:5   
     sampleSize = sum(~isnan(proxy(:,band(i).index)),2);
-    proxys = proxy_r(:,band(i).index);
+    proxy_t = proxy_sgn(:,band(i).index);
     insuffientSamples = find(sampleSize<5);
-    proxys(insuffientSamples,:) = NaN; 
+    proxy_i = proxy_t;
+    proxy_i(insuffientSamples,:) = NaN; 
+    % bin it
+    bin_i = bin_x(tce',proxy_i,binEdges);
+    comp_i       = nmean(bin_i,2);
+    pcomp_i   = reshape([comp_i comp_i]',[],1);
+    bin_t = bin_x(tce',proxy_t,binEdges);
+    comp_t       = nmean(bin_t,2);
+    pcomp_t   = reshape([comp_t comp_t]',[],1);
     subplot(5,1,i); 
-    sdx = plot(tce,nanmean(proxys,2),'color',cols(i,:))
-    ylim([floor(min(nanmean(proxys,2))) ceil(max(nanmean((proxys),2)))])
+    plot(plotEdges,pcomp_i,'color',cols(i,:),'LineWidth',2); hold on    
+    ylim([floor(min(comp)) ceil(max(comp))])
     xlim([1 2015])
     title(band(i).title)
     holdYlim = ylim;
-    line(tce,nanmean((proxy_r(:,band(i).index)),2),'color',[0.8 0.8 0.8])
-    line(tce,nanmean((proxys),2),'color',cols(i,:))
+    line(plotEdges,pcomp_t,'color',[0.8 0.8 0.8],'LineWidth',2);
+    line(plotEdges,pcomp_i,'color',cols(i,:),'LineWidth',2); 
     ylim(holdYlim)
-    set(gca,'xminortick','on','yminortick','on','box','on')
+    set(gca,'xminortick','on','yminortick','on','box','off','tickdir','out','ygrid','on')
     if i==5
         xlabel('YEAR')
     end
 end
+suptitle(['Composite by latitude, n_min = ',int2str(nmin)])
 
 set(gcf,'units','normalized','position',[0.39 0.18 0.33 0.75])
-hepta_figprint(['./figs/compositeByLatitude_' smoothString],400)
+% export
+froot = ['./figs/' opstring '_compositeByLatitude.pdf'];
+export_fig(froot,'-r300','-cmyk')
+
+%hepta_figprint(['./figs/compositeByLatitude_' smoothString],400)
 %export_fig(['./figs/compositeByLatitude_' opstring '_' smoothString  '.pdf'],'-r200')
