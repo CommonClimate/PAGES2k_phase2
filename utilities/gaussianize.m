9c5b133d4bd09ba2ad5de854c8a39319b1541819
Julien Emile-Geay <julieneg@usc.edu>
2017-03-15 23:45:04 -0700

    fixed bug in gaussianize.m and wrote pages2k_db_countryNames.py

--- utilities/gaussianize.m
+++ utilities/gaussianize.m
@@ -18,13 +18,19 @@ for j = 1:p
     [sorted,index]  = sort(Z(nz));
     % Make 'rank' the rank number of each observation
     [x, rank]       = sort(index);
-    % The cumulative distribution function
+    % Obtain the cumulative distribution function
     CDF = rank./N - 1/(2*N);
     % Apply the inverse Rosenblatt transformation
     Xn(nz,j) = sqrt(2)*erfinv(2*CDF - 1);  % Xn is now normally distributed
 end
 
 end
+%test with 30% missing data
+n = 1000;
+X = gamrnd(1,1,[n, 1]); % define highly skewed series
+X(randsample(n,round(n*.30)))= NaN;
+Xn = gaussianize(X);
+scatterhist(X,Xn,'Kernel','on')
 
 
 
