39293031a0130ef47c3ffef48927b73cad325e94
Julien Emile-Geay <julieneg@usc.edu>
2017-07-26 17:36:08 -0700

    made unpack script more rigorous... but results don't change

--- code_db/pages2k_db_unpack_AMA.m
+++ code_db/pages2k_db_unpack_AMA.m
@@ -1,19 +1,19 @@
 function  S = pages2k_db_unpack(vers,visual)
 % function pages2k__db_unpack_flat(vers)
 %  INPUT:   vers (version of the database, string)
-%  OUTPUT:  saved to PAGES2k_v{vers}_unpack.mat'
+%  OUTPUT:  saved to Pages2kPhase2Database_{vers}_unpack
 %
-%  Loads data from one of the pages2k_v{vers}.mat files and extract
+%  Loads data from one of the pages2k_vX.Y.Z.mat files and extract
 %  data and metadata for use in climate applications.
 %
-%  started   Nov 23 2015 by Julien Emile-Geay (USC), based on code by by J. Wang
-%  Updated   Feb  6 2017 for public release (JEG)
-%  Corrected July 26 2017 for seasonal averaging. 
+%  started Nov 23 2015 by Julien Emile-Geay (USC), based on code by by J. Wang
+%  Updated Feb  6 2017 for public release (JEG)
 
 % Prepare T-sensitive records
 % load data and packages
-addpath(genpath('../utilities'))
-load(['../data/PAGES2k_v' vers '.mat'])
+%addpath(genpath('../utilities'))
+load(['C:/Users/neukom/Documents/Database/PAGES2k_DB/v2_published/PAGES2k_v' vers '.mat'])
+
 
 % Keep a record if it is interpreted as temperature AND
 clim_interp = {TS.climateInterpretation_variable};
@@ -23,10 +23,6 @@ not_kept    = setdiff([1:nr],keep);  % (diagnostic purposes only)
 
 % Archive type
 archive = {S.archiveType};
-archive = strrep(archive,'tree ring','tree'); %fix Laanila archive type
-% PAGES2k ID
-pages2kID = {S.paleoData_pages2kID};
-
 
 yearMin = nan(nr,1);
 yearMax = nan(nr,1);
@@ -98,6 +94,8 @@ for r = 1:nr
         % find breaks
         [~,~,segment] = find_breaks(ti,Xi);
 
+        gfrac(r) = numel(Xn)/numel(X); % gap fraction
+
         % identify gappy records
         if nseg > 1 & resMax(r) > 10 & resMed(r) < 1 % if more than 1 segment, and large gaps
             gappy(r) = 1;
@@ -106,7 +104,7 @@ for r = 1:nr
         if resMed(r) < 1 % if subannual, coarse-grain to annual resolution
             if gappy(r)  % there are large gaps, proceed segment by segment
                 ta = unique(floor(ti))+1;
-                Xcal = NaN(size(ta)); Xdjf = Xcal; Xjja = Xcal; Xama = Xcal;
+                Xcal = NaN(size(ta)); Xdjf = Xcal; Xjja = Xcal;Xama = Xann;
                 for s = 1:nseg
                     ts = tc(segment{s}); Xs = Xc(segment{s});
                     % interpolate onto regular time grid
@@ -114,14 +112,14 @@ for r = 1:nr
                     % infill using singular spectrum analysis
                     [spec,eig_vec,PC,RC,Xi,modes] = hepta_ssam(Xr,op);
                     % intra annual averages
-                    [Zcal,tcal,~]    = intra_annual_avg(Xi,tr,1,12,1); % calendar year
-                    Xcal(ismember(ta,tcal)) = Zcal;
-                    [Zama,tama,~]  = intra_annual_avg(Xi,tr,4,3,1);  % tropical year
-                    Xama(ismember(ta,tama)) = Zama;
-                    [Zdjf,tdjf,~]  = intra_annual_avg(Xi,tr,12,2,1); % DJF
-                    Xdjf(ismember(ta,tdjf)) = Zdjf;
-                    Zjja           = intra_annual_avg(Xi,tr, 6,8,1);  % JJA
-                    Xjja(ismember(ta,tcal)) = Zjja;
+                    [cal,ty,~]  = intra_annual_avg(Xi,tr,1,12,1); % calendar year
+                    Xcal(ismember(ta,ty)) = cal;
+                    [ama,tm,~]  = intra_annual_avg(Xi,tr,4,3,1);  % tropical year
+                    Xama(ismember(ta,tm)) = ama;
+                    djf         = intra_annual_avg(Xi,tr,12,2,1); % DJF
+                    Xdjf(ismember(ta,tm)) = djf;
+                    jja         = intra_annual_avg(Xi,tr, 6,8,1);  % JJA
+                    Xjja(ismember(ta,ty)) = jja;
                 end
             else
                 % interpolate onto regular time grid
@@ -134,63 +132,62 @@ for r = 1:nr
                     Xi = Xr;
                 end
                 % intra annual averages
-                [Xcal,tcal,~] = intra_annual_avg(Xi,tr,1,12);
-                [Xama,tama,~] = intra_annual_avg(Xi,tr,4,3);
-                [Xdjf,tdjf,~] = intra_annual_avg(Xi,tr,12,2);
-                Xjja          = intra_annual_avg(Xi,tr, 6,8);
+                [Xcal,ta,~]     = intra_annual_avg(Xi,tr,1,12);
+                [Xama,tm,~]     = intra_annual_avg(Xi,tr,4,3);
+                [Xdjf,ta_djf,~] = intra_annual_avg(Xi,tr,12,2);
+                Xjja            = intra_annual_avg(Xi,tr, 6,8);
+                %for ama the output ta needs to be shifted to make the
+                %year match the Apr-Dec portion of the windows RN 2016/11/25
+                tm=tm-1;
 
-                if sum(~ismember(tcal,unique(floor(tc))))~=0
+                if sum(~ismember(ta,unique(floor(tc))))~=0
                     % make sure corals are not annualized where there were original NaNs
-                    Xcal(~ismember(tcal,unique(floor(tc)))) = nan;
-                    Xdjf(~ismember(tdjf,unique(floor(tc)))) = nan;
-                    Xjja(~ismember(tcal,unique(floor(tc)))) = nan;
-                    Xama(~ismember(tama,unique(floor(tc)))) = nan;
+                    Xcal(~ismember(ta,unique(floor(tc)))) = nan;
+                    Xdjf(~ismember(ta_djf,unique(floor(tc)))) = nan;
+                    Xjja(~ismember(ta,unique(floor(tc)))) = nan;
+                    Xama(~ismember(tm,unique(floor(tc)))) = nan;
                 end
             end
 
-            % sanity check
+            %sanita check
             if visual
                 fig('Annualization'), clf
-                set(gcf,'position',[229   0   800   600])
                 plot(tc,Xc,'marker','o','color',rgb('Gray'),'MarkerSize',8,'linestyle','none'); hold on;
-                plot(tama,Xama,'k-','linewidth',2);
+                plot(tm,Xama,'k-','linewidth',2);
                 if resMed(r) < 1
-                    plot(tdjf,Xdjf,'b-',tcal,Xjja,'r-');
-                    lab{3} = 'DJF'; lab{4} = 'JJA';
+                    plot(tm,Xdjf,'b-',ta,Xjja,'r-');
                 end
-                hold off; lab{1} = 'Raw'; lab{2} = 'Annualized'; 
-                legend(lab{:}), legend boxoff
+                hold off; lab{1} = 'Raw'; lab{2} = 'Annualized'; legend(lab{:}), legend boxoff
                 ylab = [T.paleoData_variableName ' (' removeLeadingAndTrailingSpaces(T.paleoData_units) ')'];
                 site_n = strrep(T.dataSetName,'_','\_');
                 ttl = [int2str(r),') ', archive{r},', ', site_n];
                 fancyplot_deco(ttl,'Year (CE)',ylab,14);
-                filen=['../figs/annualize/pages_2k_phase2_record_', sprintf('%03d',r) '.pdf'];
+                filen=['./figs/annualize/pages_2k_phase2_record_', sprintf('%03d',r) '.pdf'];
                 export_fig(filen,'-r100','-cmyk','-painters','-nocrop')
-                clear lab
             end
         elseif resMed(r)>=1 && resMin(r) < 1
             ta = [min(floor(tc)):max(floor(tc))]';
             F = griddedInterpolant(tc,Xc,'linear'); Xcal = F(ta);
-            Xdjf = Xcal; Xjja = Xcal;  Xama = Xcal;
-            tdjf = tcal; tama = tcal;
+            Xdjf = Xcal; Xjja = Xcal;Xama = Xcal;
+            ta_djf=ta;tm=ta;
         else
             [tc,Xc,ind] = consolidator(tn(:),Xn(:));
             ta = round(tc);
-            Xcal = Xc; Xdjf = Xc; Xjja = Xc;  Xama = Xc;
-            tdjf = tc; tama = tc;
+            Xcal = Xc; Xdjf = Xc; Xjja = Xc; Xama = Xc;
+            ta_djf=ta;tm=ta;
         end
         if strcmp(archive{r},'ice core')
-            proxy_ann(ismember(year,tcal),r) = Xcal;  % As per Eric Steig: diffusion renders subannual averages meaningless
-            proxy_djf(ismember(year,tdjf),r) = Xcal;
-            proxy_jja(ismember(year,tcal),r) = Xcal;
-            proxy_ama(ismember(year,tama),r) = Xama;
+            proxy_ann(ismember(year,ta),r) = Xcal;  % As per Eric Steig: diffusion renders subannual averages meaningless
+            proxy_djf(ismember(year,ta_djf),r) = Xcal;
+            proxy_jja(ismember(year,ta),r) = Xcal;
+            proxy_ama(ismember(year,ta_ama),r) = Xama;
         else
-            proxy_ann(ismember(year,tcal),r) = Xcal;
-            proxy_djf(ismember(year,tdjf),r) = Xdjf;
-            proxy_jja(ismember(year,tcal),r) = Xjja;
-            proxy_ama(ismember(year,tama),r) = Xama;
+            proxy_ann(ismember(year,ta),r) = Xcal;
+            proxy_djf(ismember(year,ta_djf),r) = Xdjf;
+            proxy_jja(ismember(year,ta),r) = Xjja;
+            proxy_ama(ismember(year,tm),r) = Xama;
         end
-        clear Xcal Xdjf Xjja Xama tcal tdjf tama
+        clear Xcal Xdjf Xjja Xama ta tm
     end
 end
 
@@ -216,6 +213,7 @@ for r = iWei
     proxy_ann(ismember(year,ty),r) = Xcal;
     proxy_djf(ismember(year,ty),r) = Xcal;
     proxy_jja(ismember(year,ty),r) = Xcal;
+    proxy_ama(ismember(year,ty),r) = Xcal;
 end
 % restrict to Common Era
 yearCE    = year>0;
@@ -223,7 +221,6 @@ proxy_ann = proxy_ann(yearCE,:);
 proxy_djf = proxy_djf(yearCE,:);
 proxy_jja = proxy_jja(yearCE,:);
 proxy_ama = proxy_ama(yearCE,:);
-
 year      = year(yearCE);
 
 % Coordinates
@@ -231,20 +228,15 @@ p_lat = [S.geo_meanLat];
 p_lon = [S.geo_meanLon];
 
 clear TS D tr Xr tc Xc ts Xs spec eig_vec PC RC  Xi modes Xn tn n m r Xcal
-clear gappy interval iWei noNan noNaNx nseg ny segment t T ta ti tmax
-clear tmin ty X X5 Xm clim_interp F me ms t5
+clear gappy gfrac interval iWei noNan noNaNx nseg ny segment t T ta ti tmax
+clear tmin ty X X5 Xm clim_interp F me ms
 
 recordNames = {S.dataSetName};
 vname       = {S.paleoData_variableName};
 units       = {S.paleoData_units};
 
 
-
-if ismac & ~verLessThan('matlab','R2016a') % horrible hack for JEG's machine. Mathworks, you suck!
-    save(['../data/PAGES2k_v' vers '_unpack.mat'],'-v6')
-else
-    save(['../data/PAGES2k_v' vers '_unpack.mat'])
-end
+save(['PAGES2k_v' vers '_unpack.mat'],'-v6')
 
 %save with v6 because of this:
 %http://undocumentedmatlab.com/blog/improving-save-performance
